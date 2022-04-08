64c880309f3fb3f855befd41e17af7b844b6cb84
hadrizi <vbyhgn@gmail.com>
2022-04-08 11:56:28 +0300

    refactor: proper CLI parsing

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,29 @@ mod lib;
 mod repl;
 mod tests;
 
+use clap::Parser;
 use compiler::compile;
 use lexer::tokenize;
 use lexer::tokens::Token;
 use lib::utils::LocatedResult;
 use repl::REPL;
-use std::env;
 use std::fs;
+use std::path::PathBuf;
 use std::process::exit;
+
+/// LCL programming language compiler and interactive shell
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Args {
+    /// Target file
+    #[clap(parse(from_os_str))]
+    input: Option<PathBuf>,
+
+    /// Place the output into <OUTPUT>
+    #[clap(short, long)]
+    output: Option<PathBuf>,
+}
 
 fn read_program(path: &str) -> LocatedResult<Vec<Token>> {
     let data = fs::read_to_string(path).expect("failed to read from file");
@@ -19,24 +34,25 @@ fn read_program(path: &str) -> LocatedResult<Vec<Token>> {
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        let mut repl = REPL::new(">> ");
-        repl.run_loop();
-    } else if args.len() == 2 {
-        let program = match read_program(&args[1]) {
-            Ok(p) => p,
-            Err(e) => {
+    let args = Args::parse();
+    match args.input {
+        Some(input) => {
+            // TODO: add output file support
+            let program = match read_program(input.to_str().unwrap()) {
+                Ok(p) => p,
+                Err(e) => {
+                    eprintln!("{}", e);
+                    exit(1);
+                }
+            };
+            if let Err(e) = compile(program) {
                 eprintln!("{}", e);
                 exit(1);
-            }
-        };
-        if let Err(e) = compile(program) {
-            eprintln!("{}", e);
-            exit(1);
-        };
-    } else {
-        print!("Invalid arguments");
-        exit(1);
+            };
+        }
+        _ => {
+            let mut repl = REPL::new(">> ");
+            repl.run_loop();
+        }
     }
 }
