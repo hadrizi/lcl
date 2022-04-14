60f4839b82aa10e4a9ef5a803989f6e748c448f1
hadrizi <vbyhgn@gmail.com>
2022-04-14 19:20:04 +0300

    test: compilation tests

---
+++ src/tests/compiler.rs
@@ -0,0 +1,56 @@
+#![allow(unused_imports)]
+use super::compiler_test;
+use crate::compiler::compile;
+use crate::lexer::{tokenize, tokens::Token};
+use std::fs::remove_file;
+use std::process::Command;
+use std::str::from_utf8;
+
+// Arithmetics
+compiler_test!(compile_plus, "2 2 + ." => "4\n");
+compiler_test!(compile_minus, "5 2 - ." => "3\n");
+
+// Comparison
+compiler_test!(compile_less_true, "1 2 < ." => "1\n");
+compiler_test!(compile_greater_true, "2 1 > ." => "1\n");
+compiler_test!(compile_equal_true, "2 2 = ." => "1\n");
+compiler_test!(compile_not_equal_true, "2 3 != ." => "1\n");
+compiler_test!(compile_less_false, "2 1 < ." => "0\n");
+compiler_test!(compile_greater_false, "1 2 > ." => "0\n");
+compiler_test!(compile_equal_false, "2 3 = ." => "0\n");
+compiler_test!(compile_not_equal_false, "2 2 != ." => "0\n");
+
+// Stack manipulation
+compiler_test!(compile_dup, "1 dup . ." => "1\n1\n");
+compiler_test!(compile_drop, "1 2 drop ." => "1\n");
+compiler_test!(compile_swap, "1 2 swap . ." => "1\n2\n");
+compiler_test!(compile_over, "1 2 over . . ." => "1\n2\n1\n");
+compiler_test!(compile_rot, "1 2 3 rot . . ." => "1\n3\n2\n");
+
+// Memory
+compiler_test!(compile_memory_1, "mem 1 @ mem ! ." => "1\n");
+compiler_test!(compile_memory_2, "mem 8 + 2 @ mem 8 + ! ." => "2\n");
+
+// Control flow
+compiler_test!(
+    compile_if,
+    "2 2 = if
+        1 . 
+    end" => "1\n"
+);
+compiler_test!(
+    compile_else,
+    "2 2 != if
+        1 . 
+    else 
+        2 . 
+    end" => "2\n"
+);
+compiler_test!(
+    compile_while,
+    "0
+    while dup 10 < do 
+        dup . 
+        1 + 
+    end" => "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n"
+);
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,3 +1,4 @@
+pub mod compiler;
 pub mod lexer;
 
 macro_rules! lexer_test {
@@ -28,4 +29,38 @@ macro_rules! lexer_test {
     };
 }
 
+macro_rules! compiler_test {
+    (FAIL: $name:ident, $src:expr) => {
+        #[cfg(test)]
+        #[test]
+        fn $name() {
+            let src: &str = $src;
+
+            let got = tokenize(src, "<test>");
+            assert!(got.is_err(), "{:?} should be an error", got);
+        }
+    };
+    ($name:ident, $src:expr => $should_be:expr) => {
+        #[cfg(test)]
+        #[test]
+        fn $name() {
+            let src: &str = $src;
+            let should_be = $should_be;
+            let outfile = format!("src/tests/test_{}", stringify!($name));
+
+            let tokens: Vec<Token> = tokenize(src, "<test>").unwrap();
+            compile(tokens, &outfile).unwrap();
+            let output = Command::new(&outfile).output().unwrap();
+            let result = from_utf8(&output.stdout).unwrap();
+
+            remove_file(&outfile).unwrap();
+            remove_file(format!("{}.o", &outfile)).unwrap();
+            remove_file(format!("{}.asm", &outfile)).unwrap();
+
+            assert_eq!(result, should_be, "Input was {:?}", src);
+        }
+    };
+}
+
+pub(crate) use compiler_test;
 pub(crate) use lexer_test;
